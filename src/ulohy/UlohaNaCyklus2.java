88301af21f62b070a976b59fa25ac028e7530129
janzitniak <jan.zitniak@gmail.com>
2023-08-28 20:31:16 +0200

    Úloha na cyklus č.2, resp. vypísanie matice s násobkami 2.

--- src/ulohy/UlohaNaCyklus2.java
+++ src/ulohy/UlohaNaCyklus2.java
@@ -5,8 +5,17 @@ package ulohy;
 32  64  128 256 512
 1024 ...
 */
+
 public class UlohaNaCyklus2 {
     public static void main(String[] args) {
-
+        int cislo = 1;
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(cislo + "\t");
+                //cislo *= 2;
+                cislo = cislo * 2;
+            }
+            System.out.println();
+        }
     }
 }
