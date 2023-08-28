ed35f094f4fe3409dac9739829dbddb68acfb43a
Ján Žitniak on GitHub.com <54016618+janzitniak@users.noreply.github.com>
2023-08-28 20:14:48 +0200

    Úloha 1 a 2 s využitím cyklov

---
+++ src/ulohy/UlohaNaCyklus2.java
@@ -0,0 +1,12 @@
+package ulohy;
+/*
+Úloha č.2: Vytvorte obdobnú maticu, ktorá bude obsahovať 2-násobok tej predchádzajúcej hodnoty (začneme 1), takže bude to vyzerať nasledujúco:
+ 1  2   4   8   16
+32  64  128 256 512
+1024 ...
+*/
+public class UlohaNaCyklus2 {
+    public static void main(String[] args) {
+
+    }
+}
