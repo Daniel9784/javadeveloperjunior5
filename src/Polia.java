7592bd176459b44209584251fd8af234b5a774e9
jan.zitniak <jan.zitniak@gmail.com>
2023-09-04 21:09:55 +0200

    Ako pracovať s poliami v Jave.

--- src/Polia.java
+++ src/Polia.java
@@ -1,29 +1,38 @@
 public class Polia {
     public static void main(String[] args) {
-        // Deklaria pola s nazvom cars, ktore obsahuje 4 znacky
-        String cars[] = {"Volvo", "BMW", "Ford", "Mazda"};
+        // Deklaria pola s nazvom znackyAut, ktore obsahuje 4 znacky
+        String znackyAut[] = {"Volvo", "BMW", "Ford", "Mazda"};
 
         // Vypis prvu znacku
-        System.out.println(cars[0]);
+        System.out.println(znackyAut[0]);
 
         // Vypis poslednu znacku
-        System.out.println(cars[3]);
+        System.out.println(znackyAut[3]);
 
         // Zmena znacky prveho auta
-        cars[0] = "Opel";
+        znackyAut[0] = "Opel";
         // A vypis tuto zmenu
-        System.out.println(cars[0]);
+        System.out.println(znackyAut[0]);
 
         // Pocet vsetkych znaciek aut v poli je
-        System.out.println(cars.length);
+        System.out.println(znackyAut.length);
 
         // Vypis vsetky znacky aut v poli
         System.out.println("Vypíše všetky značky áut:");
         System.out.println("-------------------------");
         // Pouzijeme cyklus s pevnym poctom opakovani a prikazu length, ktory zistuje dlzku pola
-        for (int i = 0; i < cars.length; i++) {
-            System.out.println(cars[i]);
+        for (int i = 0; i < znackyAut.length; i++) {
+            System.out.println(znackyAut[i]);
             // mozeme v cykle vykonat aj viacero prikazov
         }
+
+        System.out.println();
+        // Vypis vsetky znacky aut v poli cez foreach
+        System.out.println("Vypíše všetky značky áut cez foreach:");
+        System.out.println("-----------------------------------");
+        for (String konkretnaZnacka:znackyAut) {
+            System.out.println(konkretnaZnacka);
+        }
+
     }
 }
