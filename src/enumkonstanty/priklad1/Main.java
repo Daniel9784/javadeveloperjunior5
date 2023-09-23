f15a2ae4518a96eb64a3aec0ced12fe876ad1ea9
jan.zitniak <jan.zitniak@gmail.com>
2023-09-23 14:50:47 +0200

    Jednoduché príklady na prácu s konštantami cez enum.

---
+++ src/enumkonstanty/priklad1/Main.java
@@ -0,0 +1,7 @@
+package enumkonstanty.priklad1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Aktuálne ročné obdobie je: " + RocneObdobia.LETO);
+    }
+}
---
+++ src/enumkonstanty/priklad2/Dan.java
@@ -0,0 +1,5 @@
+package enumkonstanty.priklad2;
+
+public enum Dan {
+    // 20, 19, 18;  // Ciselne hodnoty ako konstanty Enum nemozeme pouzit
+}
